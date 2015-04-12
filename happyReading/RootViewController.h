75e069c51282e8cfb3206f37baa0ec8e877a2fe1
Yifan Xiao <xiaoyifan@wireless-s0-no-150-0-244.uchicago.edu>
2015-04-12 15:42:29 -0500

    Initial Commit

---
+++ happyReading/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  happyReading
+//
+//  Created by Yifan Xiao on 4/12/15.
+//  Copyright (c) 2015 Yifan Xiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController <UIPageViewControllerDelegate>
+
+@property (strong, nonatomic) UIPageViewController *pageViewController;
+
+@end
+
